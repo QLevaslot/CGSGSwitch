7600d6e35e466b356bbb3a8b660491d5124b318e
QLevaslot <quentin.levaslot@gmail.com>
2014-01-24 16:51:17 +0100

    [QL] Attempt to move slider on touch for mobile

--- js/class.main.js
+++ js/class.main.js
@@ -25,17 +25,14 @@ var CGMain = CGSGView.extend(
             this.rootNode = new CGSGNode(0, 0);
             CGSG.sceneGraph.addNode(this.rootNode, null);
 
-            //Text Log
+            //Text Log for first switch
             this.textNode = new CGSGNodeText(400, 20, "Value : ?");
             this.textNode.setSize(10);
             this.rootNode.addChild(this.textNode, null);
 
             //First switch
             this.switch = new CGSGNodeSwitch(15, 15, 200, 50, 50, 30);
-            /*var that = this;
-             this.switch.onClick = function (event) {
-             that.textNode.setText("Value : "+ that.switch.value);
-             }*/
+
             this.rootNode.addChild(this.switch, null);
 
             var that = this;
@@ -44,10 +41,23 @@ var CGMain = CGSGView.extend(
                 that.textNode.setText("Value : "+ that.switch.value);
             }
 
-            //First switch
+            //Text Log for second switch
+            this.textNode2 = new CGSGNodeText(400, 220, "Value : ?");
+            this.textNode2.setSize(10);
+            this.rootNode.addChild(this.textNode2, null);
+
+            //Second switch
             this.switch2 = new CGSGNodeSwitch(15, 215, 200, 50, 120, 80);
+            this.switch2.backgroundColor = "black";
+            this.switch2.handle.color = "green";
 
             this.rootNode.addChild(this.switch2, null);
+
+            this.switch2.onSwitchValueChanged = function (event) {
+
+                console.log(event);
+                that.textNode2.setText("Value : "+ that.switch2.value);
+            }
 
         }
     }
